1876f9b26254b1bb23f99e8a2d81746a8fd8dc95
Amol Samota <samota.amol.21apr@gmail.com>
2021-01-20 19:50:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,23 +10,32 @@ import TRI4 from "./assests/tri7.png";
 import TRII from "./assests/tri2.png";
 import TRIII from "./assests/tri3.png";
 import WEB from "./assests/web.png";
+import WEB2 from "./assests/web3.png";
 
 function App() {
   return (
     <div>
       <div className="App"><img src={BG}></img></div>
       <img className="roc" src={RO} ></img>
+      <button className="get">Get Started</button>
+      <img className="web" src={WEB} ></img>
+      <img className="web2" src={WEB2} ></img>
+      <img className="cir" src={CO} ></img>
+      <img className="tri" src={TRI} ></img>
+
+      {/* navbar */}
+      <div className="logo">Logo</div>
+      <div className="about">About</div>
+      <div className="services">Services</div>
+      <div className="test">Testimonials</div>
+      <div className="case">Case Studies</div>
+      <div className="contact">Contact</div>
+      <button className="login">Login</button>
 
       
-      
-        <button className="logo">Logo</button>
-        <button className="get">Get Started</button>
-        {/* <img className="web" src={WEB} ></img>
-        
-        <img className="cir" src={CO} ></img>
-        <img className="tri" src={TRI} ></img>
-        <img className="tri1" src={TRI} ></img>
-        <img className="tri2" src={TRII} ></img>
+       
+        {/* <img className="tri1" src={TRI} ></img>
+        <img className="tri2" src={TRII} ></img> 
         <img className="tri22" src={TRII} ></img> */}
         {/* <img className="tri3" src={TRIII} ></img>
         <img className="tri" src={TRI1} ></img>  */}
